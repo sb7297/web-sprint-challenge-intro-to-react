1fb6d0fd9b98ef7e370ad7fe96b37367f5bdf11f
sb7297 <sbaireddy7297@gmail.com>
2021-07-16 16:57:00 -0400

    click to expand Character

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const StyledDiv = styled.div`
@@ -15,6 +15,7 @@ const StyledDiv = styled.div`
 `;
 
 const LeftCharDiv = styled.div`
+    cursor: pointer;
     font-size: 1.2rem;
     padding: 1%;
 `;
@@ -25,15 +26,65 @@ const RightCharDiv = styled.div`
     padding: 1%;
 `;
 
+const ExpandedStyledDiv = styled(StyledDiv)`
+    flex-direction: column;
+`;
+
+const ExpandedNameDiv = styled.div`
+    cursor: pointer;
+    font-size: 1.4rem;
+    margin: 1%;
+`;
+
+const ExpandedRow = styled.div`
+    width: 80%;
+    margin-bottom: 1.5%;
+    display: flex;
+    justify-content: space-between;
+`;
+
 const Character = ({ char }) => {
+    const [expanded, setExpanded] = useState(false);
+
+    const toggleExpanded = () => {
+        setExpanded(!expanded);
+    }
+
+    if (!expanded) {
+        return (
+        <StyledDiv>
+            <LeftCharDiv onClick={toggleExpanded}>{char.name}</LeftCharDiv>
+            <RightCharDiv>
+                <div>{char.gender}</div>
+                <div>{char.birth_year}</div>
+            </RightCharDiv>
+        </StyledDiv>
+        );
+    }
     return (
-    <StyledDiv>
-        <LeftCharDiv>{char.name}</LeftCharDiv>
-        <RightCharDiv>
-            <div>{char.gender}</div>
-            <div>{char.birth_year}</div>
-        </RightCharDiv>
-    </StyledDiv>
+        <ExpandedStyledDiv>
+            <ExpandedNameDiv onClick={toggleExpanded}>{char.name}</ExpandedNameDiv>
+            <ExpandedRow>
+                <div>Gender</div>
+                <div>{char.gender}</div>
+            </ExpandedRow>
+            <ExpandedRow>
+                <div>Birth year</div>
+                <div>{char.birth_year}</div>
+            </ExpandedRow>
+            <ExpandedRow>
+                <div>Height</div>
+                <div>{char.height} cm</div>
+            </ExpandedRow>
+            <ExpandedRow>
+                <div>Mass</div>
+                <div>{char.mass}</div>
+            </ExpandedRow>
+            <ExpandedRow>
+                <div>Eye color</div>
+                <div>{char.eye_color}</div>
+            </ExpandedRow>
+        </ExpandedStyledDiv>
     );
 }
 
