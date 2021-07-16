2013feabcc10c34ba327f5359f7a74047cc430da
sb7297 <sbaireddy7297@gmail.com>
2021-07-16 16:10:29 -0400

    Character: style character item

--- src/components/Character.js
+++ src/components/Character.js
@@ -6,14 +6,33 @@ const StyledDiv = styled.div`
     margin: 2% auto;
     border: 1px solid SlateGray;
     border-radius: 0.05in;
+    box-shadow: 2px 2px 3px 0px #000000;
+    background-color: #F7F4F4;
+    color: #7E4D93;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+const LeftCharDiv = styled.div`
+    font-size: 1.6rem;
+    padding: 1%;
+`;
+
+const RightCharDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 1%;
 `;
 
 const Character = ({ char }) => {
     return (
     <StyledDiv>
-        <div>{char.name}</div>
-        <div>{char.gender}</div>
-        <div>{char.birth_year}</div>
+        <LeftCharDiv>{char.name}</LeftCharDiv>
+        <RightCharDiv>
+            <div>{char.gender}</div>
+            <div>{char.birth_year}</div>
+        </RightCharDiv>
     </StyledDiv>
     );
 }
