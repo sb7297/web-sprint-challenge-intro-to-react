8bb94783bf800fa90d43135479efae96a403fb5f
sb7297 <sbaireddy7297@gmail.com>
2021-07-16 14:04:44 -0400

    App: get characters from api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 const App = () => {
@@ -8,10 +9,21 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://swapi.dev/api/people")
+    .then(res => {
+      console.log(res);
+      setCharacters(res.data);
+    })
+    .catch(err => console.log("failed to get character data!"));
+  }, []);
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {characters.map(char => <div>{char.name}</div>)}
     </div>
   );
 }
